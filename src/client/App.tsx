477592d4f66466de270a5a3469db11df58fced91
Kristen Abby Cendana <kristencendana@gmail.com>
2023-03-02 15:59:12 -0800

    initial component setup

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,33 +1,16 @@
 import React, {useEffect} from 'react';
+import ProgramsList from './components/ProgramsList';
 
 const App = () => {
 
-  // Upon load, invoke fetchAuthentication
-  useEffect(() => {
-    fetchAuthentication();
-  }, []);
-
-  // Functionality for post request to receive authentication token
-  const fetchAuthentication = ():void => {
-    fetch('https://welbi.org/api/start', {
-    method: 'POST',
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ "email": "kristencendana@gmail.com" })
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      console.log(JSON.stringify(data));
-    })
-  }
-
-
-
   return (
-    <div className="app">Hello React App!</div>
+    <div className="app">
+      <header>Today's Programs</header>
+      <p>This works! HELLO HELLO what's up hello hello </p>
+      {/* an array list of <Programs />*/}
+      <ProgramsList />
+    </div>
+
   )
 }
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -3,21 +3,23 @@ const path = require('path');
 const app = express();
 const port = 3000;
 app.use(express.json());
-
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 
 // app.use(express.static("./dist"));
 // console.log(path.join(__dirname, '../../index.html'));
 
 // serves the CSS and JS files from dist folder
 // app.use('/dist', express.static(path.join(__dirname, '../../dist')));
-app.use(express.static(path.join(__dirname, '../client')));
+// app.use(express.static(path.join(__dirname, '../client')));
 
-// route handler to respond with main app
+// serve bundled dist folder
+app.use(express.static("./dist"));
 
 // serving the HTML file
-app.get('/', (req, res) => {
-  return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
-});
+// app.get('/', (req, res) => {
+//   return res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
+// });
 
 module.exports = app.listen(port, () => {
   console.log(`Server is listening at port https://localhost:${port}`);
