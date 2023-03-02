b20ed2fb85bfbf25958b8c8a711c3662c28585eb
Kristen Abby Cendana <kristencendana@gmail.com>
2023-03-01 16:24:55 -0800

    fetch authentication

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const App = () => {
+
+  // Upon load, invoke fetchAuthentication
+  useEffect(() => {
+    fetchAuthentication();
+  }, []);
+
+  // Functionality for post request to receive authentication token
+  const fetchAuthentication = ():void => {
+    fetch('https://welbi.org/api/start', {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "email": "kristencendana@gmail.com" })
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      console.log(JSON.stringify(data));
+    })
+  }
+
+
 
   return (
     <div className="app">Hello React App!</div>
