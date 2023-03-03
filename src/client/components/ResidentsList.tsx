4faa888ac91695119c52a540cb56b3ab8f8efe99
Kristen Abby Cendana <kristencendana@gmail.com>
2023-03-02 16:59:45 -0800

    implement context

--- src/client/components/ResidentsList.tsx
+++ src/client/components/ResidentsList.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import Resident from './Resident';
 const ResidentsList = () => {
 
   // pass down to resident??
   return (
     <div>
       Residents List
+      <button> Add New Resident </button>
+      <Resident />
     </div>
   )
 }
+
+export default ResidentsList;
---
+++ src/client/contexts/DataContext.tsx
@@ -0,0 +1,5 @@
+import React, { createContext, useContext, useState } from 'react';
+
+// Types interface for Context
+// Types interface for Program
+
