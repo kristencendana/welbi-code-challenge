9ec045e9dd6bd7e1b37e46b62807ba3bc0c41fe2
Kristen Abby Cendana <kristencendana@gmail.com>
2023-03-02 16:59:45 -0800

    implement context

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -5,9 +5,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <header>Today's Programs</header>
-      <p>This works! HELLO HELLO what's up hello hello </p>
-      {/* an array list of <Programs />*/}
+      <header>Welbi</header>
       <Navbar/>
       <ComponentContainer/>
     </div>
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./style.scss";
 
-let root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(<App />)
